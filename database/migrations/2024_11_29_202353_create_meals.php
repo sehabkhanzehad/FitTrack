9eef1d5987e6da9f0d9ef2a4189c54c8b68c0280
intakhabchy <intakhab.chy@gmail.com>
2024-11-30 02:54:58 +0600

    3 tables migration

---
+++ database/migrations/2024_11_29_202353_create_meals.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('meals', function (Blueprint $table) {
+            $table->id();
+            $table->string('meal_name');
+            $table->string('created_by');
+            $table->timestamp('created_at')->useCurrent();
+            $table->string('updated_by');
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('meals');
+    }
+};
---
+++ database/migrations/2024_11_29_205336_create_diet_plans.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('diet_plans', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('food_id');
+            $table->unsignedBigInteger('meal_id');
+            $table->time('priority');
+            $table->string('carb_qty');
+            $table->string('created_by');
+            $table->timestamp('created_at')->useCurrent();
+            $table->string('updated_by');
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+
+            $table->foreign('food_id')->references('id')->on('foods')->cascadeOnUpdate()->restrictOnDelete();
+            $table->foreign('meal_id')->references('id')->on('meals')->cascadeOnUpdate()->restrictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('diet_plans');
+    }
+};
