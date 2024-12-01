40d670fd57a88bfabe9e8b72b5de0310c636da37
Mosaddequr Rahman Khan <emon2020@outlook.com>
2024-12-01 23:04:17 +0600

    web_php routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+//use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserWorkoutProgressController;
+
+Route::prefix('user-workout-progresses')->group(function () {
+    Route::get('/', [UserWorkoutProgressController::class, 'index'])->name('userWorkoutProgresses.index');
+    Route::post('/', [UserWorkoutProgressController::class, 'store'])->name('userWorkoutProgresses.store');
+    Route::get('/{id}', [UserWorkoutProgressController::class, 'show'])->name('userWorkoutProgresses.show');
+    Route::put('/{id}', [UserWorkoutProgressController::class, 'update'])->name('userWorkoutProgresses.update');
+    Route::delete('/{id}', [UserWorkoutProgressController::class, 'destroy'])->name('userWorkoutProgresses.destroy');
 });
+
